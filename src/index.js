cfe05c0aee87f9b76cf8a1955413d736da94d40e
niamhtalbot11 <74904383+niamhtalbot11@users.noreply.github.com>
2023-04-18 13:18:20 +0200

    converted temperature features
    added fahrenheit link

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ function formatDate(date) {
    document.querySelector(".weatherDescription").innerHTML = response.data.weather[0].description;
    document.querySelector(".humidity").innerHTML= Math.round(response.data.main.humidity);
    document.querySelector(".windSpeed").innerHTML = Math.round(response.data.wind.speed);
+   celsiusTemperature = response.data.main.temp;
    iconElement = document.querySelector("#mainIcon");
    iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
    );
@@ -43,7 +44,7 @@ function formatDate(date) {
     let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
   }
- 
+  searchFunction("Dublin");
   function retrievePosition(position){
    console.log(position.coords.lat);
    console.log(position.coords.lon);
@@ -58,11 +59,38 @@ function formatDate(date) {
    event.preventDefault();
    navigator.geolocation.getCurrentPosition(retrievePosition);
   }
- 
+
+function convertToFahrenheit(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+
+let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function convertToCelsius(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
  let form = document.querySelector(".searchForm");
  form.addEventListener("submit",search);
 
 let currentLocationButton = document.querySelector(".currentLocationButton")
 currentLocationButton.addEventListener("click", showCurrentLocation);
 
-searchFunction("Dublin");
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+
