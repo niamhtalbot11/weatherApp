62e4299a9641c8d260801e434d9eaadcb2f12a25
niamhtalbot11 <74904383+niamhtalbot11@users.noreply.github.com>
2023-04-21 12:44:28 +0200

    working on weekly forecast

--- src/index.js
+++ src/index.js
@@ -18,21 +18,80 @@ function formatDate(date) {
  
   let now = new Date();
   document.querySelector(".currentDate").innerHTML = formatDate(now);
- 
-  //Geolocation
- 
-  function displayWeather(response){
-   document.querySelector(".city").innerHTML = response.data.name;
-   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
-   document.querySelector(".weatherDescription").innerHTML = response.data.weather[0].description;
-   document.querySelector(".humidity").innerHTML= Math.round(response.data.main.humidity);
-   document.querySelector(".windSpeed").innerHTML = Math.round(response.data.wind.speed);
-   celsiusTemperature = response.data.main.temp;
-   iconElement = document.querySelector("#mainIcon");
-   iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-   );
- 
+
+
+
+// day for weekly forecast
+  function formatDay(timestamp){
+let date = new Date (timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed","Thur","Fri","Sat"]
+return days[day];
   }
+
+
+function displayForecast(response){
+  console.log(response.data.daily);
+let forecast = response.data.daily;
+
+let forecastElement = document.querySelector("#forecast");
+    
+    let forecastHTML =`<div class="row">`;
+    forecast.forEach(function (forecastDay, index){
+      if (index < 6) {
+      forecastHTML =
+      forecastHTML + 
+      `
+
+      <div class="col-2">
+      <div class="weather-forecast-day">${formatDay(forecastDay.time)}</div>
+      <img
+      src="
+      http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+      alt=""
+      width="42"
+      id="#forecast-img"
+    />
+
+    <div class="weather-forecast-temperatures">
+
+    <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}
+    º</span>
+      
+    <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}
+    º</span>
+        
+    </div>
+    </div>
+      `;
+      }
+      });
+    forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey ="41c1d9b9a93t238a49ob3d17fa62cf09";
+  let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayForecast);
+}
+console.log(forecastDay);
+ //Geolocation
+ function displayWeather(response){
+  document.querySelector(".city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector(".weatherDescription").innerHTML = response.data.weather[0].description;
+  document.querySelector(".humidity").innerHTML= Math.round(response.data.main.humidity);
+  document.querySelector(".windSpeed").innerHTML = Math.round(response.data.wind.speed);
+  celsiusTemperature = response.data.main.temp;
+  iconElement = document.querySelector("#mainIcon");
+  iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+   );
+   getForecast(response.data.coord);
+   }
+  
+  //city input
   function search(event) {
     event.preventDefault();
    let cityInputElement = document.querySelector("#searchBar").value;
@@ -44,7 +103,11 @@ function formatDate(date) {
     let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
   }
+
   searchFunction("Dublin");
+
+  //current location api 
+
   function retrievePosition(position){
    console.log(position.coords.lat);
    console.log(position.coords.lon);
@@ -54,11 +117,14 @@ function formatDate(date) {
    let apiUrl =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
   }
+
  //current location button
   function showCurrentLocation(event){
    event.preventDefault();
    navigator.geolocation.getCurrentPosition(retrievePosition);
   }
+
+//convert to fahrenheit
 
 function convertToFahrenheit(event){
   event.preventDefault();
@@ -69,6 +135,7 @@ function convertToFahrenheit(event){
 let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
 temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+//convert to celsius
 
 function convertToCelsius(event){
   event.preventDefault();
@@ -81,8 +148,10 @@ function convertToCelsius(event){
 
 let celsiusTemperature = null;
 
- let form = document.querySelector(".searchForm");
- form.addEventListener("submit",search);
+
+
+let form = document.querySelector(".searchForm");
+form.addEventListener("submit",search);
 
 let currentLocationButton = document.querySelector(".currentLocationButton")
 currentLocationButton.addEventListener("click", showCurrentLocation);
